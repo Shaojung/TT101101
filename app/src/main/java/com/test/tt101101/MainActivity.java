6ba0333691a378591f54e38010177b327c9711b0
Shaojung <shaojung@gmail.com>
2016-10-11 10:19:25 +0800

    Add String array and layout.

--- app/src/main/java/com/test/tt101101/MainActivity.java
+++ app/src/main/java/com/test/tt101101/MainActivity.java
@@ -1,7 +1,9 @@
 package com.test.tt101101;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -10,11 +12,12 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     ListView lv;
-
+    String[] data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        data = new String[] {"AA", "BBB", "CC22", "DD44", "EEEEE555"};
         lv = (ListView) findViewById(R.id.listView);
 
         MyAdapter adapter = new MyAdapter();
@@ -26,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return 5;
+            return data.length;
         }
 
         @Override
@@ -41,9 +44,12 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            TextView tv = new TextView(MainActivity.this);
-            tv.setText("Hello:" + position);
-            return tv;
+            LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+            View v = inflater.inflate(R.layout.myitem, null);
+
+            TextView tv = (TextView) v.findViewById(R.id.textView);
+            tv.setText(data[position] + ":" + position);
+            return v;
         }
     }
 

5cf322293875ae2e7029eab214d310f7987ff02c
Shaojung <shaojung@gmail.com>
2016-10-11 11:29:38 +0800

    Use Student class

--- app/src/main/java/com/test/tt101101/MainActivity.java
+++ app/src/main/java/com/test/tt101101/MainActivity.java
@@ -10,14 +10,22 @@ import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     ListView lv;
     String[] data;
+    ArrayList<Student> mylist;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         data = new String[] {"AA", "BBB", "CC22", "DD44", "EEEEE555", "FF","GG","HH","II","LL", "FF1","GG2","HH3","II4","LL5"};
+        mylist.add(new Student("AA", "11111"));
+        mylist.add(new Student("BB", "2222222"));
+        mylist.add(new Student("CC", "333333"));
+        mylist.add(new Student("DD", "4444444"));
+
         lv = (ListView) findViewById(R.id.listView);
 
         MyAdapter adapter = new MyAdapter(MainActivity.this, data);
---
+++ app/src/main/java/com/test/tt101101/Student.java
@@ -0,0 +1,16 @@
+package com.test.tt101101;
+
+/**
+ * Created by teacher on 2016/10/11.
+ */
+
+public class Student {
+    String name;
+    String tel;
+
+    public Student(String n, String t)
+    {
+        this.name = n;
+        this.tel = t;
+    }
+}
